8e5ed0102af485f1e88ceb6a49f52431d9241bd7
shanur <shanur.rahman@gamechangesns.com>
2020-06-03 01:10:33 +0400

    matching ports

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ io.on("connection", (socket) => {
 });
 
 
-const port = process.env.port || 3000
+const port = process.env.port || 8080
 server.listen(port, () => {
   console.log(`listening on *:${port}`);
 });
